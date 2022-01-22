59e48a025b257d8f7ba099a03cc45a9604059bee
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2022-01-22 14:49:14 -0500

    added test animation, and most of clock vectors

--- app/src/main/java/com/example/physicalalarm/MainActivity.java
+++ app/src/main/java/com/example/physicalalarm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.physicalalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
 
 import android.app.Activity;
 import android.media.Image;
@@ -16,14 +18,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        View clockFace = findViewById(R.id.clock_wedges);
         ImageView topLeftWedge = findViewById(R.id.top_left_wedge);
         ImageView topRightWedge = findViewById(R.id.top_right_wedge);
         ImageView bottomLeftWedge = findViewById(R.id.bottom_left_wedge);
         ImageView bottomRightWedge = findViewById(R.id.bottom_right_wedge);
+        final SpringAnimation anim1X = new SpringAnimation(clockFace,
+                DynamicAnimation.ROTATION);
         topLeftWedge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                anim1X.animateToFinalPosition(45f);
                 Toast.makeText(getApplicationContext(), "9-12 quadrant pressed", Toast.LENGTH_SHORT).show();
             }
         });
