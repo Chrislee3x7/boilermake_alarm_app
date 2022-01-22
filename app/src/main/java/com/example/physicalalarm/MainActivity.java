d8a40c5f4a47df577b3a4d034dd7e1527225fdc6
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2022-01-22 01:45:53 -0500

    making gridlayout for main page work

--- app/src/main/java/com/example/physicalalarm/MainActivity.java
+++ app/src/main/java/com/example/physicalalarm/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.physicalalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button topLeftWedge = findViewById(R.id.top_left_wedge);
+        Button topRightWedge = findViewById(R.id.top_right_wedge);
+        Button bottomLeftWedge = findViewById(R.id.bottom_left_wedge);
+        Button bottomRightWedge = findViewById(R.id.bottom_right_wedge);
+        topLeftWedge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "9-12 quadrant pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        topRightWedge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "12-3 quadrant pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomLeftWedge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "6-9 quadrant pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomRightWedge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "3-6 quadrant pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
