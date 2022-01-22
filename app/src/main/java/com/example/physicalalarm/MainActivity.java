eff9f37a31da216481dc98fbf7b0eac26275fed1
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2022-01-22 16:35:37 -0500

    animation for top left quadrant complete

--- app/src/main/java/com/example/physicalalarm/MainActivity.java
+++ app/src/main/java/com/example/physicalalarm/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.physicalalarm;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 
 import android.app.Activity;
 import android.media.Image;
@@ -14,21 +14,47 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    private View clockFace;
+    private View clockTicksAndNumbers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View clockFace = findViewById(R.id.clock_wedges);
+        clockFace = findViewById(R.id.clock_face);
         ImageView topLeftWedge = findViewById(R.id.top_left_wedge);
         ImageView topRightWedge = findViewById(R.id.top_right_wedge);
         ImageView bottomLeftWedge = findViewById(R.id.bottom_left_wedge);
         ImageView bottomRightWedge = findViewById(R.id.bottom_right_wedge);
-        final SpringAnimation anim1X = new SpringAnimation(clockFace,
-                DynamicAnimation.ROTATION);
+
+
+
         topLeftWedge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                anim1X.animateToFinalPosition(45f);
+                final SpringAnimation rotate = new SpringAnimation(clockFace,
+                        DynamicAnimation.ROTATION);
+                final SpringAnimation panDownAnimY  = new SpringAnimation(clockFace, DynamicAnimation.TRANSLATION_Y);
+                SpringForce springForce = new SpringForce();
+                panDownAnimY.setSpring(springForce);
+                panDownAnimY.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                    @Override
+                    public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+//                        animateZoomIn();
+
+                    }
+                });
+                springForce.setStiffness(100f);
+                springForce.setDampingRatio(1);
+                rotate.setSpring(springForce);
+                rotate.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                    @Override
+                    public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                        animateZoomIn();
+                        panDownAnimY.animateToFinalPosition(1500f);
+                    }
+                });
+                rotate.animateToFinalPosition(45f);
                 Toast.makeText(getApplicationContext(), "9-12 quadrant pressed", Toast.LENGTH_SHORT).show();
             }
         });
@@ -50,6 +76,16 @@ public class MainActivity extends Activity {
                 Toast.makeText(getApplicationContext(), "3-6 quadrant pressed", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void animateZoomIn() {
+        final SpringAnimation zoomToTopAnimX = new SpringAnimation(clockFace, DynamicAnimation.SCALE_X);
+        final SpringAnimation zoomToTopAnimY = new SpringAnimation(clockFace, DynamicAnimation.SCALE_Y);
+        SpringForce springForce = new SpringForce();
+        springForce.setStiffness(100f);
+        springForce.setDampingRatio(1);
+        zoomToTopAnimY.animateToFinalPosition(4f);
+        zoomToTopAnimX.animateToFinalPosition(4f);
     }
 
     @Override
